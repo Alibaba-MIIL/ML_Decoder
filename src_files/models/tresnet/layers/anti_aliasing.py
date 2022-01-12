e94a5ea1f1abf96051af2d107c57b45ef57ca533
talrid <tal.ridnik@alibaba-inc.com>
2022-01-12 18:09:35 +0200

    clear Downsample

--- src_files/models/tresnet/layers/anti_aliasing.py
+++ src_files/models/tresnet/layers/anti_aliasing.py
@@ -6,21 +6,20 @@ import torch.nn.functional as F
 
 
 class AntiAliasDownsampleLayer(nn.Module):
-    def __init__(self, remove_model_jit: bool = False, filt_size: int = 3, stride: int = 2,
+    def __init__(self, filt_size: int = 3, stride: int = 2,
                  channels: int = 0):
         super(AntiAliasDownsampleLayer, self).__init__()
-        if not remove_model_jit:
-            self.op = DownsampleJIT(filt_size, stride, channels)
-        else:
-            self.op = Downsample(filt_size, stride, channels)
+        self.op = Downsample(filt_size, stride, channels)
 
     def forward(self, x):
         return self.op(x)
 
 
-@torch.jit.script
-class DownsampleJIT(object):
+# @torch.jit.script
+class Downsample(nn.Module):
     def __init__(self, filt_size: int = 3, stride: int = 2, channels: int = 0):
+        super(Downsample, self).__init__()
+
         self.stride = stride
         self.filt_size = filt_size
         self.channels = channels
@@ -31,30 +30,30 @@ class DownsampleJIT(object):
 
         filt = (a[:, None] * a[None, :]).clone().detach()
         filt = filt / torch.sum(filt)
-        self.filt = filt[None, None, :, :].repeat((self.channels, 1, 1, 1)).cuda().half()
+        filt = filt[None, None, :, :].repeat((self.channels, 1, 1, 1))
+        self.register_buffer('filt', filt, persistent=False)
 
-    def __call__(self, input: torch.Tensor):
-        if input.dtype != self.filt.dtype:
-            self.filt = self.filt.float() 
+    def forward(self, input: torch.Tensor):
+        self.filt = self.filt.to(device=input.device, dtype=input.dtype)
         input_pad = F.pad(input, (1, 1, 1, 1), 'reflect')
         return F.conv2d(input_pad, self.filt, stride=2, padding=0, groups=input.shape[1])
 
 
-class Downsample(nn.Module):
-    def __init__(self, filt_size=3, stride=2, channels=None):
-        super(Downsample, self).__init__()
-        self.filt_size = filt_size
-        self.stride = stride
-        self.channels = channels
-
-
-        assert self.filt_size == 3
-        a = torch.tensor([1., 2., 1.])
-
-        filt = (a[:, None] * a[None, :]).clone().detach()
-        filt = filt / torch.sum(filt)
-        self.filt = filt[None, None, :, :].repeat((self.channels, 1, 1, 1))
-
-    def forward(self, input):
-        input_pad = F.pad(input, (1, 1, 1, 1), 'reflect')
-        return F.conv2d(input_pad, self.filt, stride=self.stride, padding=0, groups=input.shape[1])
+# class Downsample(nn.Module):
+#     def __init__(self, filt_size=3, stride=2, channels=None):
+#         super(Downsample, self).__init__()
+#         self.filt_size = filt_size
+#         self.stride = stride
+#         self.channels = channels
+#
+#
+#         assert self.filt_size == 3
+#         a = torch.tensor([1., 2., 1.])
+#
+#         filt = (a[:, None] * a[None, :]).clone().detach()
+#         filt = filt / torch.sum(filt)
+#         self.filt = filt[None, None, :, :].repeat((self.channels, 1, 1, 1))
+#
+#     def forward(self, input):
+#         input_pad = F.pad(input, (1, 1, 1, 1), 'reflect')
+#         return F.conv2d(input_pad, self.filt, stride=self.stride, padding=0, groups=input.shape[1])
